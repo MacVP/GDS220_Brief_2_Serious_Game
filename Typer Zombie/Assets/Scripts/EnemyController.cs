31c1c623e3df36d1f868a53671ccd14ab2b595c3
MacVP <48820988+MacVP@users.noreply.github.com>
2019-10-01 10:12:39 +0800

    Update

---
+++ Typer Zombie/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public float speed;
+    private GameObject target;
+    private GameObject sprite;
+
+    void Awake()
+    {
+        target = GameObject.FindGameObjectWithTag("Player");
+        sprite = gameObject.transform.Find("Enemy").gameObject;
+    }
+
+    void Update()
+    {               
+        LookAtPlayer();
+        MoveToPlayer();
+    }
+
+    //Makes enemy always look atr the player
+    void LookAtPlayer()
+    {
+        //What is the difference in position?
+        Vector3 diff = (target.transform.position - sprite.transform.position);
+
+        //We use aTan2 since it handles negative numbers and division by zero errors. 
+        float angle = Mathf.Atan2(diff.y, diff.x);
+
+        //Now we set our new rotation. 
+        sprite.transform.rotation = Quaternion.Euler(0f, 0f, angle * Mathf.Rad2Deg);
+    }
+
+    //Mkaes the enemy move towards the player
+    void MoveToPlayer()
+    {
+       transform.position = Vector2.MoveTowards(transform.position, target.transform.position, speed * Time.deltaTime);      
+    }
+
+
+}
--- Typer Zombie/Assets/Scripts/WordSpawner.cs
+++ Typer Zombie/Assets/Scripts/WordSpawner.cs
@@ -6,10 +6,17 @@ public class WordSpawner : MonoBehaviour
 {
     public GameObject wordPrefab;
     public Transform wordCanvas;
+    public GameObject enemyPrefab;
+    public Transform enemyPos;
+
+    private void Awake()
+    {
+        enemyPos = wordPrefab.GetComponentInChildren<Transform>(); 
+    }
 
     public WordDisplay SpawnWord()
     {
-        GameObject wordObject = Instantiate(wordPrefab);
+        GameObject wordObject = Instantiate(wordPrefab, wordCanvas);
         WordDisplay wordDisplay = wordObject.GetComponent<WordDisplay>();
 
         return wordDisplay;
